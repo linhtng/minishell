73a25f8ffc3763f6b05d6dc208f26cf7c8e2060a
Joonas Henriksson <joonas.henriksson@gmail.com>
2023-05-30 15:25:48 +0300

    exec: Run builtin commands in pipe as subprocesses
    As per bash reference[1]:
    
    "Each command in a multi-command pipeline, where pipes are created,
    is executed in its own subshell, which is a separate process."
    
    [1] https://www.gnu.org/software/bash/manual/bash.html#Pipelines

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -6,7 +6,7 @@
 /*   By: jhenriks <jhenriks@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 21:11:53 by jhenriks          #+#    #+#             */
-/*   Updated: 2023/05/29 20:22:05 by jhenriks         ###   ########.fr       */
+/*   Updated: 2023/05/30 15:24:28 by jhenriks         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,23 +29,46 @@ static int	exec_builtin(t_cmd *cmd, t_list	*env_list)
 	return (ret);
 }
 
-static int	exec_path(t_cmd *cmd, char *cmd_path, char **envp)
-{
-	sigquit_child();
-	if (!redirect_streams(cmd->write_fd, cmd->read_fd))
-		exit(1);
-	if (execve(cmd_path, cmd->argv, envp) == -1)
-		print_error(4, "exec error: ", cmd->pathname, ": ", strerror(errno));
-	exit(1);
-}
-
-void	exec(t_cmd *cmd, t_list	*env_list, char **cmd_path, char ***envp)
+static void	exec_builtin_child(t_cmd *cmd, t_list	*env_list)
 {
 	pid_t	child;
-	int		status;
 
+	child = fork();
+	if (child == 0)
+	{
+		if (!redirect_streams(cmd->write_fd, cmd->read_fd))
+			exit (1);
+		else
+			exit (run_builtin(env_list, cmd->pathname, cmd->argv));
+	}
+	waitpid(child, &g_exit_status, 0);
+}
+
+static void	exec_path(t_cmd *cmd, char *cmd_path, char **envp)
+{
+	int		status;
+	pid_t	child;
+
+	child = fork();
+	if (child == 0)
+	{
+		sigquit_child();
+		if (!redirect_streams(cmd->write_fd, cmd->read_fd))
+			exit(1);
+		if (execve(cmd_path, cmd->argv, envp) == -1)
+			print_error(4, "exec error:",
+				cmd->pathname, ": ", strerror(errno));
+		exit(1);
+	}
+	waitpid(child, &status, 0);
+	if (WIFEXITED(status))
+		g_exit_status = WEXITSTATUS(status);
+}
+
+static void	exec(t_cmd *cmd, t_list	*env_list, char **cmd_path, char ***envp)
+{
 	if (cmd_is_builtin(cmd->pathname))
-		g_exit_status = exec_builtin(cmd, env_list);
+		exec_builtin_child(cmd, env_list);
 	else
 	{
 		*cmd_path = expand_path(env_list, cmd->pathname);
@@ -57,12 +80,7 @@ void	exec(t_cmd *cmd, t_list	*env_list, char **cmd_path, char ***envp)
 		}
 		*envp = env_list_to_array(env_list);
 		setup_signals_child();
-		child = fork();
-		if (child == 0)
-			exec_path(cmd, *cmd_path, *envp);
-		waitpid(child, &status, 0);
-		if (WIFEXITED(status))
-			g_exit_status = WEXITSTATUS(status);
+		exec_path(cmd, *cmd_path, *envp);
 	}
 }
 
@@ -72,17 +90,23 @@ void	executor(t_list	*env_list, t_list *cmd_list)
 	char	*cmd_path;
 	char	**envp;
 
-	while (cmd_list)
+	cmd = cmd_list->content;
+	if (!cmd_list->next && cmd_is_builtin(cmd->pathname))
+		g_exit_status = exec_builtin(cmd, env_list);
+	else
 	{
-		cmd = cmd_list->content;
-		envp = NULL;
-		cmd_path = NULL;
-		exec(cmd, env_list, &cmd_path, &envp);
-		close_redirects(&cmd->write_fd, &cmd->read_fd);
-		if (envp)
-			free_env_array(envp);
-		if (cmd_path)
-			free(cmd_path);
-		cmd_list = cmd_list->next;
+		while (cmd_list)
+		{
+			cmd = cmd_list->content;
+			envp = NULL;
+			cmd_path = NULL;
+			exec(cmd, env_list, &cmd_path, &envp);
+			close_redirects(&cmd->write_fd, &cmd->read_fd);
+			if (envp)
+				free_env_array(envp);
+			if (cmd_path)
+				free(cmd_path);
+			cmd_list = cmd_list->next;
+		}
 	}
 }
