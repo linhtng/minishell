17489798fc9f5fc912fb9029a0e530101c004c87
Joonas Henriksson <joonas.henriksson@gmail.com>
2023-05-30 17:48:54 +0300

    builtins/exit: Fix argument handling

--- src/builtin/exit.c
+++ src/builtin/exit.c
@@ -6,7 +6,7 @@
 /*   By: jhenriks <jhenriks@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 17:49:29 by jhenriks          #+#    #+#             */
-/*   Updated: 2023/05/29 18:48:09 by jhenriks         ###   ########.fr       */
+/*   Updated: 2023/05/30 17:48:37 by jhenriks         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,24 @@ int	exit_shell(t_list **env_list, char **args)
 {
 	int	status;
 
-	if (args[1])
-	{
-		print_error(2, "exit: ", "too many arguments");
-		return (1);
-	}
 	status = 0;
-	clear_env_list(env_list);
 	ft_putstr_fd("exit\n", 1);
-	if (*args)
+	if (args[0])
 	{
-		if (!ft_isnumstr(*args))
+		if (!ft_isnumstr(args[0]))
 		{
-			print_error(4, "exit: ", *args, ": ", "numeric argument required");
-			exit(255);
+			print_error(4, "exit: ", args[0], ": ",
+				"numeric argument required");
+			status = 255;
 		}
-		status = (unsigned char)ft_atoi(*args);
+		else if (args[1])
+		{
+			print_error(2, "exit: ", "too many arguments");
+			return (1);
+		}
+		else
+			status = (unsigned char)ft_atoi(args[0]);
 	}
+	clear_env_list(env_list);
 	exit(status);
 }
