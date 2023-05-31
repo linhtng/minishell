40b4006928bec7d37a9514b755916e2d699a3502
Joonas Henriksson <joonas.henriksson@gmail.com>
2023-05-31 17:46:42 +0300

    env_list_to_array: Use ft_strmjoin for creating array strings
    Looks cleaner, and also fixes norm error for too many lines.

--- src/env/env_export.c
+++ src/env/env_export.c
@@ -6,7 +6,7 @@
 /*   By: jhenriks <jhenriks@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 21:58:53 by jhenriks          #+#    #+#             */
-/*   Updated: 2023/05/16 19:25:29 by jhenriks         ###   ########.fr       */
+/*   Updated: 2023/05/31 17:44:47 by jhenriks         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,6 @@ char	**env_list_to_array(t_list *env_list)
 	char	**envp;
 	char	**envptr;
 	char	**envvar;
-	size_t	len;
 
 	envp = (char **)malloc(sizeof(char *) * (ft_lstsize(env_list) + 1));
 	if (!envp)
@@ -45,15 +44,12 @@ char	**env_list_to_array(t_list *env_list)
 	while (env_list)
 	{
 		envvar = (char **)env_list->content;
-		len = ft_strlen(envvar[0]) + ft_strlen(envvar[1]) + 2;
-		*envptr = (char *)malloc(sizeof(char) * len);
+		*envptr = ft_strmjoin(3, envvar[0], "=", envvar[1]);
 		if (!*envptr)
+		{
 			free_env_array(envp);
-		if (!*envptr)
 			return (NULL);
-		ft_strlcat(*envptr, envvar[0], len);
-		ft_strlcat(*envptr, "=", len);
-		ft_strlcat(*envptr, envvar[1], len);
+		}
 		env_list = env_list->next;
 		envptr++;
 	}
