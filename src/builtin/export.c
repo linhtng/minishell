d60e4159f6954404d9c5b2ebbde801b69e2f8784
Joonas Henriksson <joonas.henriksson@gmail.com>
2023-06-03 22:34:29 +0300

    builtin/export: Fix norm for too many lines in function

--- src/builtin/export.c
+++ src/builtin/export.c
@@ -6,19 +6,28 @@
 /*   By: jhenriks <jhenriks@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 20:21:50 by jhenriks          #+#    #+#             */
-/*   Updated: 2023/06/03 20:28:34 by jhenriks         ###   ########.fr       */
+/*   Updated: 2023/06/03 22:34:10 by jhenriks         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static void	add_envvar(t_list **env_list, char **var)
+{
+	t_list	*var_node;
+
+	var_node = find_envvar(env_list, var[0]);
+	if (var_node && var[1])
+		del_envvar(env_list, var_node);
+	if (!var_node || (var_node && var[1]))
+		ft_lstadd_back(env_list, ft_lstnew(var));
+}
+
 // export builtin
-// TODO print_full_env if no arguments given???
 int	export(t_list **env_list, char **args)
 {
 	int		retval;
 	char	**var;
-	t_list	*var_node;
 
 	retval = 0;
 	while (*args)
@@ -26,19 +35,14 @@ int	export(t_list **env_list, char **args)
 		var = parse_variable(*args);
 		if (!var)
 			return (1);
-		if (!check_envvar_name(var[0]))
+		if (check_envvar_name(var[0]))
+			add_envvar(env_list, var);
+		else
 		{
 			print_error(4, "export: ", *args, ": ", "not a valid identifier");
 			free_envvar(var);
-			args++;
 			retval = 1;
-			continue ;
 		}
-		var_node = find_envvar(env_list, var[0]);
-		if (var_node && var[1])
-			del_envvar(env_list, var_node);
-		if (!var_node || (var_node && var[1]))
-			ft_lstadd_back(env_list, ft_lstnew(var));
 		args++;
 	}
 	return (retval);
